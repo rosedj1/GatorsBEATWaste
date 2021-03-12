f71907fb5e96ee2e74baba705ffb76543a363cc4
Jake <rosedj1@gmail.com>
2021-03-11 21:40:38 -0500

    Calculated diversion rate of 38.5%

--- test_class.py
+++
@@ -1,9 +0,0 @@
-class PlasticItem:
-    """A class to manage plastic items."""
-    def __init__(self, weight, name):
-        self.weight = weight
-        self.name = name
-
-    def get_weight(self):
-        """Return the weight of this item."""
-        return self.weight
